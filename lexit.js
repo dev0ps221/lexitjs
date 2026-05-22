6b34d6c903d65e8d51f76e011cdb6c290c15c338
dev0ps221 <seybalofficiel@gmail.com>
2026-05-22 13:17:16 +0000

    minimum lexing

--- lexit.js
+++ lexit.js
@@ -4,8 +4,36 @@
 expression = "2 + 4 = 5"
 tests = {
     number:/[0-9]/,
-    string:/[a-z]/i,
+    identifier:/[a-z]/i,
     space:/ /
+}
+blocks = [
+    {
+        type:"STRING",
+        delimiters:[
+            {
+                start:"\"",
+                end:"\""
+            },
+            {
+                start:"'",
+                end:"'"
+            },
+        ]
+    },
+    {
+        type:"FUNC",
+        delimiters:[
+            {
+                start:"func",
+                end:"end"
+            }
+        ]
+    }
+]
+signs = {
+    "\"" : "DOUBLEQUOTE",
+    "\'" : "QUOTE",
 }
 operators = {
     "+"  : "PLUS",
@@ -22,7 +50,9 @@ keywords = [
     "say",
     "get",
     "func",
+    "end",
     "let",
+    "do",
     "do",
     "while",
     "if",
@@ -60,16 +90,16 @@ function lexit(expression)
             values.push(['number',value])
             continue
         }
-        if(tests.string.test(char))
+        if(tests.identifier.test(char))
         {
             char = chars[cursor]
-            while(tests.string.test(char))
+            while(tests.identifier.test(char))
             {
                 value+=char
                 cursor++
                 char = chars[cursor]
             }
-            values.push(['string',value])
+            values.push(['identifier',value])
             continue
         }
         if(operators.hasOwnProperty(char))
@@ -77,8 +107,89 @@ function lexit(expression)
             values.push([operators[char],value])
             continue
         }
+        // if(signs.hasOwnProperty(char))
+        // {
+        //     values.push([signs[char],value])
+        //     let block_index = 0
+        //     let block_size = blocks.length
+        //     let block = null
+        //     let delimiter = null
+        //     while(block_index < block_size)
+        //     {
+        //         let delimiters_index = 0
+        //         let delimiters_size = blocks[block_index].delimiters.length
+        //         console.info(delimiters_index < delimiters_size)
+        //         while(delimiters_index < delimiters_size)
+        //         {
+        //             if(blocks[block_index].delimiters[delimiters_index].start.match(char))
+        //             {
+        //                 block = blocks[block_index]
+        //                 value = ""
+        //                 cursor++
+        //                 char = chars[cursor]
+        //                 while(char != blocks[block_index].delimiters[delimiters_index].end.match(char))
+        //                 {
+        //                     value+= char
+        //                     cursor++
+        //                     char = chars[cursor]
+        //                 }
+        //                 values.push([block.type,value])
+        //                 break
+        //             }
+        //             delimiters_index++    
+        //             if(block)
+        //             {
+        //                 break
+        //             }
+        //         }
+        //         block_index++
+        //         if(block)
+        //         {
+        //             break
+        //         }
+        //     }
+        //     continue
+        // }
+
+        if (signs.hasOwnProperty(char)) {
+            values.push([signs[char], char])
+
+            let delimiter = null
+
+            for (let b = 0; b < blocks.length; b++) {
+                for (let d = 0; d < blocks[b].delimiters.length; d++) {
+                    if (blocks[b].delimiters[d].start === char) {
+                        delimiter = blocks[b].delimiters[d]
+                        break
+                    }
+                }
+                if (delimiter) break
+            }
+
+            if (delimiter) {
+                let value = ""
+                char = chars[cursor]
+
+                while (cursor < chunk_size && char !== delimiter.end) {
+                    value += char
+                    cursor++
+                    char = chars[cursor]
+                }
+
+                values.push(["STRING", value])
+
+                if (char === delimiter.end) {
+                    values.push([signs[char], char])
+                    cursor++
+                }
+
+                continue
+            }
+        }
         tokens_count++
     }
     console.info(values)
 }
+// lexit(expression)
+expression = '"salut comment tu vas"'
 lexit(expression)
