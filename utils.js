567836596fa4b3b926870c4181785125355bbb62
dev0ps221 <seybalofficiel@gmail.com>
2026-05-24 07:56:00 +0000

    minimum basics

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const fs            = require('fs')
 const os            = require('os')
-const current_path  = (sub=null)=>'./'+(sub?"/"+sub:"")
+const current_path  = (sub=null)=>'.'+(sub?(sub.startsWith('/') ? "" : "/")+sub:"")
 const read_json     = (filepath)=>JSON.parse(fs.readFileSync(filepath))
 const assets_path   = (sub=null)=>current_path('assets')+(sub?"/"+sub:"")
 
